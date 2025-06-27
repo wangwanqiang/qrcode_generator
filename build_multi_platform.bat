5918313058c0d96f7cc0b3dbf11b85336109e2d5
wwq <wwq100@gmail.com>
2025-06-27 20:08:38 +0800

    add mod

--- build_multi_platform.bat
+++ build_multi_platform.bat
@@ -3,6 +3,8 @@ setlocal
 
 echo ========== 开始跨平台编译二维码生成程序 ==========
 
+go mod init
+
 :: 确保当前目录有 go.mod 文件
 if not exist go.mod (
     echo 错误: 当前目录缺少 go.mod 文件，请先执行 go mod init
@@ -19,14 +21,14 @@ mkdir bin
 echo 正在编译 Windows 64位版本...
 set GOOS=windows
 set GOARCH=amd64
-go build -ldflags="-w -s" -o bin/qrcode_generator_windows_amd64.exe .\qrcode_generator.go
+go build -ldflags="-w -s" -o bin/qrcode_generator_windows_amd64.exe .\main.go
 if errorlevel 1 goto :compile_error
 
 :: 编译 Linux 64位版本
 echo 正在编译 Linux 64位版本...
 set GOOS=linux
 set GOARCH=amd64
-go build -ldflags="-w -s" -o bin/qrcode_generator_linux_amd64 .\qrcode_generator.go
+go build -ldflags="-w -s" -o bin/qrcode_generator_linux_amd64 .\main.go
 if errorlevel 1 goto :compile_error
 
 :: 编译 ARM 32位版本 (ARMv7)
@@ -34,14 +36,14 @@ echo 正在编译 ARM 32位版本...
 set GOOS=linux
 set GOARCH=arm
 set GOARM=7
-go build -ldflags="-w -s" -o bin/qrcode_generator_armv7 .\qrcode_generator.go
+go build -ldflags="-w -s" -o bin/qrcode_generator_armv7 .\main.go
 if errorlevel 1 goto :compile_error
 
 :: 编译 ARM 64位版本 (AARCH64)
 echo 正在编译 ARM 64位版本...
 set GOOS=linux
 set GOARCH=arm64
-go build -ldflags="-w -s" -o bin/qrcode_generator_arm64 .\qrcode_generator.go
+go build -ldflags="-w -s" -o bin/qrcode_generator_arm64 .\main.go
 if errorlevel 1 goto :compile_error
 
 echo ========== 编译完成 ==========
